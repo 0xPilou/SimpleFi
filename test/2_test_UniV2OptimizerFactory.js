813371d923641d7d778c61ccf1177c71f47c7650
PierreLvl <pierre.louvel@protonmail.com>
2021-09-16 20:58:23 +0200

    preparation for : optimizerFactory tests

---
+++ test/2_test_UniV2OptimizerFactory.js
@@ -0,0 +1,28 @@
+/**
+ *  Dependencies
+ */
+ const { expect } = require("chai");
+ const { ethers } = require("hardhat");
+ const truffleAssert = require('truffle-assertions');
+
+describe("UniV2OptimizerFactory Unit Tests", function () {
+   
+    before(async () => {
+    });
+
+    it("should add a new strategy to the UniV2Optimizer Factory ", async () => {
+        
+    });
+    
+    it("should get the number of strategy supported", async () => {
+    
+    });
+
+    it("should get the number of Optimizer(s) created", async () => {
+
+    });
+
+    it("should create a new Optimizer with Strategy ID 2", async () => {
+
+    });
+});
