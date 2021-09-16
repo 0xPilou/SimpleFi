2ac8ccdab5e023d07d79af23912339c478de5377
PierreLvl <pierre.louvel@protonmail.com>
2021-09-16 12:46:39 +0200

    code cleanup - add comments

--- test/1_test_UniV2Optimizer.js
+++ test/1_test_UniV2Optimizer.js
@@ -4,17 +4,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const truffleAssert = require('truffle-assertions');
-const fs = require('fs');
 
-// const alchemyApiKey = fs.readFileSync("./secret").toString().trim();
-
-/**
- *  ABIs
-*/
-   
 describe("UniV2Optimizer Unit Tests", function () {  
     
-
+    /* ABIs */
     const StakingAbi = require("./external_abi/Staking.json");
     const RewardAbi = require("./external_abi/Reward.json");
     const TokenAAbi = require("./external_abi/TokenA.json");
@@ -23,9 +16,7 @@ describe("UniV2Optimizer Unit Tests", function () {
     const StakingRewardAbi = require("./external_abi/StakingReward.json");
     const UniswapV2RouterAbi = require("./external_abi/Router.json");
 
-    /**
-     *  Adresses
-    */
+    /* Adresses */
     const StakingAddress = "0x80676b414a905De269D0ac593322Af821b683B92";
     const RewardAddress =  "0x9C78EE466D6Cb57A4d01Fd887D2b5dFb2D46288f";
     const TokenAAddress = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
@@ -34,8 +25,10 @@ describe("UniV2Optimizer Unit Tests", function () {
     const StakingRewardAddress = "0x2328c83431a29613b1780706E0Af3679E3D04afd";
     const UniswapV2RouterAddress = "0x93bcDc45f7e62f89a8e901DC4A0E2c6C427D9F25";
 
+    /* Provider */
     const provider = new ethers.providers.JsonRpcProvider();
 
+    // Instantiating the existing mainnet fork contracts
     staking = new ethers.Contract(StakingAddress, StakingAbi, provider);
     reward = new ethers.Contract(RewardAddress, RewardAbi, provider);
     tokenA = new ethers.Contract(TokenAAddress, TokenAAbi, provider);
@@ -53,6 +46,7 @@ describe("UniV2Optimizer Unit Tests", function () {
         // MUST-MATIC LP Whale  : 0xdA8479E5b8A273A403148a779Fbb8903DC2C739d
         // WMATIC Whale         : 0x84D34f4f83a87596Cd3FB6887cFf8F17Bf5A7B83
 
+        // Impersonating a whale address to provision the owner account with necessary tokens
         await hre.network.provider.request({
             method: "hardhat_impersonateAccount",
             params: ["0xdA8479E5b8A273A403148a779Fbb8903DC2C739d"],
@@ -60,6 +54,7 @@ describe("UniV2Optimizer Unit Tests", function () {
         whale = await ethers.getSigner("0xdA8479E5b8A273A403148a779Fbb8903DC2C739d");   
         [owner, addr1, addr2, _] = await ethers.getSigners();   
 
+        // Deploying the contract under test
         UniV2Optimizer = await ethers.getContractFactory("UniV2Optimizer");
         uniV2Optimizer = await UniV2Optimizer.connect(owner).deploy(
             tokenA.address,
@@ -69,29 +64,33 @@ describe("UniV2Optimizer Unit Tests", function () {
             stakingReward.address,
             uniV2Router.address
         );
-
-
     });
 
     it("should stake 10 LP tokens to the Staking Reward Pool", async () => {
 
-
+        // Quantity to deposit for this test
         const amountToDeposit = 10;
         const weiAmountToDeposit = ethers.utils.parseEther(amountToDeposit.toString());
 
+        // Transfering 10 LP tokens from a whale address to the UniV2Optimizer owner address
         await staking.connect(whale).transfer(owner.address, weiAmountToDeposit);
 
+        // Saving the balances before the staking operation
         const userBalBefore = await staking.balanceOf(owner.address);      
         const poolBalBefore = await staking.balanceOf(stakingReward.address);
         
+        // Approving the 10 LP tokens to be spent by the UniV2Optimizer
         await staking.connect(owner).approve(uniV2Optimizer.address, weiAmountToDeposit);
         await uniV2Optimizer.connect(owner).stake(weiAmountToDeposit);
         
+        // Saving the balances after the staking operation
         const userBalAfter = await staking.balanceOf(owner.address);      
         const poolBalAfter = await staking.balanceOf(stakingReward.address);      
 
-        
+        // Assertion : Staking Pool Balance After = Staking Pool Balance Before + 10
         expect(poolBalAfter).to.equal(poolBalBefore.add(weiAmountToDeposit))
+
+        // Assertion : User Balance Before = User Balance Before - 10
         expect(userBalAfter).to.equal(userBalBefore.sub(weiAmountToDeposit))
 
     });
