d5b5de029aebfec0ff6b82611b1b2b587202a5e7
PierreLvl <pierre.louvel@protonmail.com>
2021-09-19 20:20:55 +0200

    correct typo raised during test campaign - 4_test_AmmZapFactory.js

--- contracts/AmmZapFactory.sol
+++ contracts/AmmZapFactory.sol
@@ -1,21 +1,22 @@
 pragma solidity ^0.8.0;
 
 import "./AmmZap.sol";
+import 'openzeppelin-solidity/contracts/access/Ownable.sol';
+
 
 contract AmmZapFactory is Ownable {
     address[] public ammZaps;
     mapping(address => address) public zapRouterRegistry; 
 
-    
     function createAmmZap(address _ammRouterAddr) external onlyOwner returns(address newAmmZap) {
-        require(zapRouterRegistry[_ammRouterAddr] == 0, "This router already has an AmmZap deployed.");
+        require(zapRouterRegistry[_ammRouterAddr] == address(0), "This router already has an AmmZap deployed.");
         AmmZap ammZap = new AmmZap(_ammRouterAddr);
         ammZaps.push(address(ammZap));
         zapRouterRegistry[_ammRouterAddr] = address(ammZap);
         return address(ammZap);
     }
 
-    function getAmmZapByRouter(_ammRouterAddr) external view returns(address) {
+    function getAmmZapByRouter(address _ammRouterAddr) external view returns(address) {
         return zapRouterRegistry[_ammRouterAddr];
     }
 
@@ -23,6 +24,4 @@ contract AmmZapFactory is Ownable {
         return ammZaps.length;
     } 
 }
-
-
 
