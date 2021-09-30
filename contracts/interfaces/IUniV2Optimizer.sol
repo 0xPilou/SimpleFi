e38f372e30d162347a919e5c4ff8337710e39bd0
PierreLvl <pierre.louvel@protonmail.com>
2021-09-30 17:42:42 +0200

    update IUniV2Optmizer interface

--- contracts/interfaces/IUniV2Optimizer.sol
+++ contracts/interfaces/IUniV2Optimizer.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.8.0;
 
 interface IUniV2Optimizer {
   
-    function staked() external view returns (uint256);
+    function tokenA() external view returns (address);
+    function tokenB() external view returns (address);
     function staking() external view returns (address);
+    function reward() external view returns (address);
+    function stakingRewardAddr() external view returns (address);
+    function uniV2RouterAddr() external view returns (address);
     function ammZapAddr() external view returns (address);
+    function feeCollector() external view returns (address);
+    function parentFactory() external view returns (address);
+
+    function staked() external view returns (uint256);
     
+    function zapAndStake(address _tokenToZap, uint256 _amountToZap) external;
     function stake(uint256 _amount) external;
     
     function withdraw(uint256 _amount) external;
